7a9a515daaa395375e4a107a3a2caf9333ce9551
Blackscure <Wekesabuyahi@gmail.com>
2018-08-16 16:45:36 +0300

    my work

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from database import Database
+from models.post import Post 
+
+Database.initialize()
+
+post = Post(blog_id="123",
+            title="Another great post",
+            content="This is some sample content",
+            author="Jose")
+
+
+post.save_to_mongo()
