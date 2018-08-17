422d7950d9454f83b2aa0400d35113a276d59db3
Blackscure <Wekesabuyahi@gmail.com>
2018-08-17 14:38:52 +0300

    changes made

--- src/app.py
+++ src/app.py
@@ -1,19 +1,11 @@
 from database import Database
 from models.post import Post 
 from models.blog import Blog 
+from menu import Menu
 
 
 Database.initialize()
 
-blog = Blog(author="Jose", 
-            title="sample title",
-            description="sample description")
+menu = Menu()
 
-
-blog.new_post()
-
-blog.save_to_mongo()
-
-from_database = Blog.from_mongo(blog.id)
-
-print(blog.get_posts())
+menu.run_menu()
